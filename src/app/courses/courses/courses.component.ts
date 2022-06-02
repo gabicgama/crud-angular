86ddb93328d6c6bf0a40868f4d9d079f19fc4de8
gabriela.gama <gabycgama@hotmail.com>
2022-06-02 13:46:54 -0300

    adicionando chamada HTTP GET e rxjs

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { CoursesService } from './../services/courses.service';
 import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,18 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   //coursesService: CoursesService;
 
   constructor(private coursesService: CoursesService) {
     //this.coursesService = new CoursesService();
-
+    this.courses = this.coursesService.list();
   }
 
   ngOnInit(): void {
-    this.courses = this.coursesService.list();
   }
 
 }
